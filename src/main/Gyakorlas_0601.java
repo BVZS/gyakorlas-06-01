ae7cbb2588d48b02370fae9682ec986250a5785e
Balazs Fajd <mrfajd@gmail.com>
2024-06-01 11:13:18 +0200

    sorozat elemeinek kiirasa, progtetelek bemutatasa

--- src/main/Gyakorlas_0601.java
+++ src/main/Gyakorlas_0601.java
@@ -7,24 +7,52 @@ public class Gyakorlas_0601 {
         feladatok();
     }
 
-    private static int[] sorozat = {3, 4, 0, 12, 1, 8};
+    private static final String SEP = System.lineSeparator();
+    private static int[] sorozat = {32, 11, 0, 12, 1, -5, 8};
     
     private static void feladatok() {
-        osszegzes();
-        megszamlalas();
-        maximumKivalasztas();
-        minimumKivalasztas();
-        kivalasztas();
-        eldontesEgy();
-        eldontesMind();
-        linearisKereses();
-        
-        kiir("osszegzes: " + osszegzes() + "\n");
-        kiir("megszamlalas: " + megszamlalas() + "\n");
-        kiir("maximumKivalasztas: " + maximumKivalasztas() + "\n");
-        kiir("minimumKivalasztas: " + minimumKivalasztas() + "\n");
-        kiir("kivalasztas: " + kivalasztas() + "\n");
+        kiir("A sorozat elemei: ");
+        kiir("[ " + sorozatElemei() + "]" + SEP);
+        progTetelekFelhasznalasa();
     }
+    
+    private static void progTetelekFelhasznalasa() {
+        osszegzesBemutatasa();
+        megszamlalasBemutatasa();
+        szelsoertekekBemutatas();
+        kivalasztasBemutatasa();
+        eldontesekBemutatasa();
+        linKerBemutatasa();
+    }
+        
+    private static void osszegzesBemutatasa() {
+        kiir("[Összegzés] A sorozat elemeit összeadjuk és eredményül kapunk: " + osszegzes() + SEP);
+    }
+    
+    private static void megszamlalasBemutatasa() {
+        kiir("[Megszámlálás] Megnézzük, hogy a sorozat hány elemből áll: " + megszamlalas() + SEP);
+    }
+    
+    private static void szelsoertekekBemutatas() {
+        kiir("[Minimum] Megnézzük a sorozat legkisebb elemének értékét: " + minimumKivalasztas() + SEP);
+        kiir("[Maximum] Megnézzük a sorozat legnagyobb elemének értékét: " + maximumKivalasztas() + SEP);
+    }
+    
+    private static void kivalasztasBemutatasa() {
+        kiir("[Kiválasztás] Megnézzük a sorozatban az első olyan elemet amely kisebb egy meghatározott értéknél (3): érték: " + sorozat[kivalasztas()] + " index: " + kivalasztas() + SEP);
+    }
+    
+    private static void eldontesekBemutatasa() {
+        kiir("[Eldöntés: Egy] Megnézzük a sorozatban, hogy van-e legalább egy olyan elem, amely nagyobb egy meghatározott értéknél (20): " + eldontesEgy() + SEP);
+        kiir("[Eldöntés: Mind] Megnézzük a sorozatban, hogy minden elem nagyobb-e, egy meghatározott értéknél (5): " + eldontesMind() + SEP);
+    }
+    
+    private static void linKerBemutatasa() {
+        kiir("[Lineáris keresés] Megnézzük a sorozatban, hogy van-e olyan elem, amely nagyobb egy meghatározott értéknél (3): " + linearisKereses() + SEP);
+    }
+    
+    
+    
     
     public static int osszegzes() {
         int osszead = 0;
@@ -45,7 +73,7 @@ public class Gyakorlas_0601 {
         return db;
     }
 
-    private static int minimumKivalasztas() {
+    private static int maximumKivalasztas() {
         int maxIndex = 0;
         for (int i = 1; i < sorozat.length; i++) {
             if(sorozat[i] > sorozat[maxIndex]){
@@ -55,7 +83,7 @@ public class Gyakorlas_0601 {
         return sorozat[maxIndex];
     }
     
-    private static int maximumKivalasztas() {
+    private static int minimumKivalasztas() {
        int minIndex = 0;
         for (int i = 1; i < sorozat.length; i++) {
             if(sorozat[i] < sorozat[minIndex]){
@@ -67,7 +95,7 @@ public class Gyakorlas_0601 {
     
     // csak addig, amíg nincs meg az eredmény
     private static int kivalasztas() {
-        int ker = 1;
+        int ker = 3;
         int i = 0;
         while(!(sorozat[i] < ker)){
             i++;
@@ -76,7 +104,7 @@ public class Gyakorlas_0601 {
     }
     
     private static boolean eldontesEgy() {
-        int ker = 1;
+        int ker = 20;
         int i = 0;
         while(i < sorozat.length && !(sorozat[i] > ker)){
             i++;
@@ -85,7 +113,7 @@ public class Gyakorlas_0601 {
     }
     
     private static boolean eldontesMind() {
-        int ker = 2;
+        int ker = 5;
         int i = 0;
         while(i < sorozat.length && (sorozat[i] > ker)){
             i++;
@@ -99,13 +127,15 @@ public class Gyakorlas_0601 {
         while(i < sorozat.length && !(sorozat[i] > ker)){
             i++;
         }
-        boolean van = i < sorozat.length;  
-        /*if(van) {
-            System.out.printf("A keresett elem (> %d):\n", ker);
-        } else {
-            System.out.println("Nincs a keresésnek megfelelő elem!");
-        }*/
-        return van;
+        return i < sorozat.length;
+    }
+    
+    private static String sorozatElemei() {
+        String szoveg = "";
+        for (int i = 0; i < sorozat.length; i++) {
+            szoveg = szoveg + sorozat[i] + " ";
+        }
+        return szoveg;
     }
     
     private static void kiir(String szoveg) {
