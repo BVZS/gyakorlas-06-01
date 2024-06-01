18ae241bec953ec6fda66a0fbf9bed5bd269515d
Balazs Fajd <mrfajd@gmail.com>
2024-06-01 09:54:34 +0200

    parameterek torolve, osszes progtetel elkeszitve

--- src/main/Gyakorlas_0601.java
+++ src/main/Gyakorlas_0601.java
@@ -7,54 +7,100 @@ public class Gyakorlas_0601 {
         feladatok();
     }
 
+    private static int[] sorozat = {0, 1, 2, 3};
+    
     private static void feladatok() {
-        int[] sorozat = {0, 1, 2, 3};
-        
-        osszegzes(sorozat);
-        megszamlalas(sorozat);
-        maximumKivalasztas(sorozat);
-        minimumKivalasztas(sorozat);
-        kivalasztas(sorozat);
+        osszegzes();
+        megszamlalas();
+        maximumKivalasztas();
+        minimumKivalasztas();
+        kivalasztas();
+        eldontesEgy();
+        eldontesMind();
+        linearisKereses();
     }
     
-    public static int osszegzes(int[] tomb) {
+    public static int osszegzes() {
         int osszead = 0;
-        for (int i = 0; i < tomb.length; i++) {
-            osszead += tomb[i];
+        for (int i = 0; i < sorozat.length; i++) {
+            osszead += sorozat[i];
         }
         return osszead;
     }
 
     // az összes elemet végig kell nézni
-    private static void megszamlalas(int[] sorozat) {
-        
+    private static int megszamlalas() {
+        int db = 0;
+        for (int i = 0; i < sorozat.length; i++) {
+            if (sorozat[i] > 0) {
+                db++;
+            }
+        }
+        return db;
     }
 
-    private static void minimumKivalasztas(int[] sorozat) {
-        
+    private static int minimumKivalasztas() {
+        int maxIndex = 0;
+        for (int i = 1; i < sorozat.length; i++) {
+            if(sorozat[i] > sorozat[maxIndex]){
+                maxIndex = i;
+            }
+        }
+        return sorozat[maxIndex];
     }
     
-    private static void maximumKivalasztas(int[] sorozat) {
-       
+    private static int maximumKivalasztas() {
+       int minIndex = 0;
+        for (int i = 1; i < sorozat.length; i++) {
+            if(sorozat[i] < sorozat[minIndex]){
+                minIndex = i;
+            }
+        }
+        return sorozat[minIndex];
     }
     
     // csak addig, amíg nincs meg az eredmény
-    private static int kivalasztas(int[] sorozat) {
-        return 0;
+    private static int kivalasztas() {
+        int ker = 1;
+        int i = 0;
+        while(!(sorozat[i] < ker)){
+            i++;
+        }
+        return sorozat[i];  
     }
     
-    private static boolean eldontesEgy(int[] sorozat) {
-        return false;
+    private static boolean eldontesEgy() {
+        int ker = 1;
+        int i = 0;
+        while(i < sorozat.length && !(sorozat[i] > ker)){
+            i++;
+        }
+        return i < sorozat.length;  
     }
     
-    private static boolean eldontesMind(int[] sorozat) {
-        return false;
+    private static boolean eldontesMind() {
+        int ker = 2;
+        int i = 0;
+        while(i < sorozat.length && (sorozat[i] > ker)){
+            i++;
+        }
+        return i >= sorozat.length;
     }
     
-    private static int linearisKereses(int[] sorozat) {
-        return -1;
+    private static boolean linearisKereses() {
+        int ker = 3;
+        int i = 0;
+        while(i < sorozat.length && !(sorozat[i] > ker)){
+            i++;
+        }
+        boolean van = i < sorozat.length;  
+        /*if(van) {
+            System.out.printf("A keresett elem (> %d):\n", ker);
+        } else {
+            System.out.println("Nincs a keresésnek megfelelő elem!");
+        }*/
+        return van;
     }
-    
     
     private static void kiir(String szoveg) {
         System.out.print(szoveg);
